746fa66428ae189a61c822d0a1a7c4ca8205a7ba
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2019-05-19 18:20:55 +0900

    Create items dynamically

--- src/lights_out/core.cljs
+++ src/lights_out/core.cljs
@@ -1,33 +1,50 @@
 (ns ^:figwheel-hooks lights-out.core
   (:require [clojure.string :as str]))
 
+(defn create-items [parent num-col]
+  (let [style (str "flex-basis: " (/ 100 num-col) "%;")]
+    (doseq [i (range num-col)
+            j (range num-col)]
+      (let [elem (doto (js/document.createElement "div")
+                   (.setAttribute "id" (str/join "-" ["item" i j]))
+                   (.setAttribute "style" style)
+                   (.classList.add "item"))]
+        (set! (.-textContent elem) "X")
+        (.appendChild parent elem)))))
+
 (defn item-vec []
   (->> (js/document.querySelectorAll ".item")
        array-seq
        vec))
 
 (defn toggle-light! [item]
-  (let [current (.-innerHTML item)]
-    (set! (.-innerHTML item) (if (= current "O") "X" "O"))))
+  (let [current (.-textContent item)]
+    (set! (.-textContent item) (if (= current "O") "X" "O"))))
 
 (defn initialise! [items]
   (run! #(when (zero? (rand-int 2)) (toggle-light! %))
         items))
 
 (defn completed? [items]
-  (every? #(= (.-innerHTML %) "X") items))
+  (every? #(= (.-textContent %) "X") items))
 
-(defn calculate-target-indices [item]
+(defn calculate-target-indices [item num-col]
   (let [[row column] (->> (str/split (.-id item) #"-")
                           rest
-                          (map #(js/parseInt % 10)))]
+                          (map #(js/parseInt % 10)))
+        max-col-idx (dec num-col)]
     (keep (fn [[r c]]
-            (when (and (<= 0 r 4) (<= 0 c 4))
-              (+ (* 5 r) c)))
+            (when (and (<= 0 r max-col-idx)
+                       (<= 0 c max-col-idx))
+              (+ (* num-col r) c)))
           [[(dec row) column]
            [row (dec column)]
            [row (inc column)]
            [(inc row) column]])))
+
+(def number-of-columns 5)
+
+(create-items (js/document.getElementById "app") number-of-columns)
 
 (initialise! (item-vec))
 
@@ -39,6 +56,6 @@
            items (item-vec)]
        (toggle-light! target)
        (run! #(toggle-light! (nth items %))
-             (calculate-target-indices target))
+             (calculate-target-indices target number-of-columns))
        (when (completed? items)
          (js/alert "Completed 🎉"))))))
