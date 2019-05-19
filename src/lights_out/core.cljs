c8f118ed79cf877444a360453f1e5c3715dc05d9
Kent OHASHI <ignorantia.juris.non.excusa@gmail.com>
2019-05-19 15:56:43 +0900

    Intial implementation

--- src/lights_out/core.cljs
+++ src/lights_out/core.cljs
@@ -1,23 +1,44 @@
 (ns ^:figwheel-hooks lights-out.core
-  (:require
-   [goog.dom :as gdom]))
+  (:require [clojure.string :as str]))
 
-(println "This text is printed from src/lights_out/core.cljs. Go ahead and edit it and see reloading in action.")
+(defn item-vec []
+  (->> (js/document.querySelectorAll ".item")
+       array-seq
+       vec))
 
-(defn multiply [a b] (* a b))
+(defn toggle-light! [item]
+  (let [current (.-innerHTML item)]
+    (set! (.-innerHTML item) (if (= current "O") "X" "O"))))
 
+(defn initialise! [items]
+  (run! #(when (zero? (rand-int 2)) (toggle-light! %))
+        items))
 
-;; define your app data so that it doesn't get over-written on reload
-(defonce app-state (atom {:text "Hello world!"}))
+(defn completed? [items]
+  (every? #(= (.-innerHTML %) "X") items))
 
-(defn get-app-element []
-  (gdom/getElement "app"))
+(defn calculate-target-indices [item]
+  (let [[row column] (->> (str/split (.-id item) #"-")
+                          rest
+                          (map #(js/parseInt % 10)))]
+    (keep (fn [[r c]]
+            (when (and (<= 0 r 4) (<= 0 c 4))
+              (+ (* 5 r) c)))
+          [[(dec row) column]
+           [row (dec column)]
+           [row (inc column)]
+           [(inc row) column]])))
 
+(initialise! (item-vec))
 
-
-;; specify reload hook with ^;after-load metadata
-(defn ^:after-load on-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
+(doseq [item (item-vec)]
+  (.addEventListener
+   item "click"
+   (fn [e]
+     (let [target (.-target e)
+           items (item-vec)]
+       (toggle-light! target)
+       (run! #(toggle-light! (nth items %))
+             (calculate-target-indices target))
+       (when (completed? items)
+         (js/alert "Completed 🎉"))))))
